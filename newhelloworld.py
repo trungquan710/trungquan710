cae6df61d5814ca3e7689a8ba68c7cdb2e508197
trungquan710 <40234184+trungquan710@users.noreply.github.com>
2022-04-12 12:29:23 +0700

    Create newhelloworld.py
    Copy from Python folder

---
+++ newhelloworld.py
@@ -0,0 +1,78 @@
+print()
+print(' _    _ ______ _      _      ____   __          ______  _____  _      _____  ')
+print('| |  | |  ____| |    | |    / __ \  \ \        / / __ \|  __ \| |    |  __ \ ')
+print('| |__| | |__  | |    | |   | |  | |  \ \  /\  / / |  | | |__) | |    | |  | |')
+print('|  __  |  __| | |    | |   | |  | |   \ \/  \/ /| |  | |  _  /| |    | |  | |')
+print('| |  | | |____| |____| |___| |__| |    \  /\  / | |__| | | \ \| |____| |__| |')
+print('|_|  |_|______|______|______\____/      \/  \/   \____/|_|  \_\______|_____/ ')
+print('                                                                             ')
+
+#num=int(input("Please enter a number: "))
+#if num>18:
+#    print("The number is greater than 18")
+#elif num<18:
+#    print("The number is less than 18")
+#else:
+#    print("The number is equal to 18")
+
+
+a=10;print(a)
+
+a=20
+b=1.2
+c="PythonGeeks"
+d= True
+print("The datatype of a is", type(a))
+print("The datatype of b is", type(b))
+print("The datatype of c is", type(c))
+print("The datatype of d is", type(d))
+
+a=10
+b=20
+sum=a+b
+print("the sum of {0} and {1} is {2}".format(a, b, sum))
+
+a=10;b=20;sum=10+20
+print(f"the sum of {a} and {b} is {sum}")
+
+a=10
+b=20
+sum=a+b
+print("the sum of %s and %s is %s"% (a,b,sum))
+
+#num=int(input("enter a number: "))
+#sum=0
+#for i in range(0, num):
+#    sum=sum+i
+#print("the sum is ",sum)
+#if sum>20:
+#    print("the sum is greater than 20")
+#elif sum<20:
+#    print("the sum is less than 20")
+#else:
+#    print("the sum is equal to 20")
+print()
+
+import time
+print('Hello, Welcome to PythonGeeks')
+print('the next message will be printed in 3 seconds')
+time.sleep(3)
+print('\n\nSleep time was 3 seconds')
+
+#side=int(input("enter the side of the square\n"))
+#def areasquare(side):
+#    """ Takes in the length of the side of the square
+#    as input
+#    and calculates the area"""
+#    print("The area of the square is", side**2)
+#areasquare(side)
+
+"""
+asdasd
+asdas
+da
+sd
+asdas
+"""
+
+print('bye bye')
